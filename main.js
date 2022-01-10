dc2e6411f0597b4a3d0ea9178ed35312ca6f5e11
dmitryure@gmail.com <dmitryure@gmail.com>
2022-01-10 15:49:29 +0300

    added some logic

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
+const host = window.location.hostname.replace("www.", "");
+chrome.storage.sync.get(host, (items) => {
+  items?.[host]?.map((el) => {
+    console.log(el, window.location.pathname);
+    if (window.location.pathname.includes(el)) {
+      document.body.innerHTML = "hi";
+    }
+  });
 });
